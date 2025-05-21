9118ff63460ea9b9a4cfb4ec2a5f6048e4eb4833
JatScript <158581128+patrajatin0@users.noreply.github.com>
2025-05-21 05:24:41 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+// in old js there is not presented module, fuctions,clssses,arrow fuction
+
+"use strict"; //  terate all js code as newer version
+// alert ("hello") we are using node.js not browser
+
+console.log(3
+    +
+ 3);//code readability should be high
+
+ console.log("Hitesh");
+ let name = "hitesh"
+ let age = 18
+ let isLoggedIn = false
+ 
+ 
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,3 @@
 console.log("Hitesh")
+
+const userName = "jatin0@"
