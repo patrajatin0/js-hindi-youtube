8e8c06f9f4b3f6d055e9a2acb12dd899ac95c6f0
JatScript <158581128+patrajatin0@users.noreply.github.com>
2025-05-21 06:30:00 +0000

    01-arrya

---
+++ 02_basics/01_array.js
@@ -0,0 +1,48 @@
+// Array
+
+const myArr = [0,1,2,3,4,5]
+const myHeors = ["shaktiman","naagraj"]
+
+const myArr2 =  new Array (1,2,3,4)
+// console.log(myArr[1]);
+
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(9)
+// myArr.pop()
+
+
+// myArr.unshift("jatin")
+// myArr.shift()
+
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(3));
+
+
+// const newArr = myArr.join()
+
+// console.log(typeof newArr);
+// console.log(myArr);
+
+
+
+// slice , splice 
+
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3)
+ 
+// console.log(myn1);
+
+// console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c",myArr);
+
+console.log(myn2);
+
+
